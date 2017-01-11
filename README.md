2dff6270aa35dc6cfb4fcb2b7220c511dcc7fbcd
SkayLarsen <SkayLarsen@gmail.com>
2017-01-11 21:17:11 +0300

    задача #18

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Price Formatter
 
-[TODO. There will be project description]
+Скрипт приводит запись цены товара в конкретный, удобный для отображения формат.
+
+Допускает два варианта использования:
+* Прямой запуск из консоли
+```bash
+python format_price.py price
+```
+* Импортирование функции проверки в сторонний скрипт:
+
+```
+from format_price import format_price
+```
+
+Также, включён набор тестов для проверки корректности работы скрипта. Тесты доступны в файле tests.py
+```
+python tests.py
+```
 
 # Project Goals
 
--- format_price.py
+++ format_price.py
@@ -1,5 +1,18 @@
+import argparse
+
 def format_price(price):
-    pass
+    price = price.replace(',', '.')
+    try:
+        price = float(price)
+    except ValueError:
+        return 'Ошибка'
+    if price.is_integer():
+        return '{0:,.0f}'.format(price).replace(',', ' ')
+    else:
+        return '{0:,.2f}'.format(price).replace(',', ' ')
 
 if __name__ == '__main__':
-    pass
+    argparser = argparse.ArgumentParser(description='Форматирование записи цены')
+    argparser.add_argument('price', type=str, help='строка с ценой')
+    args = argparser.parse_args()
+    print(format_price(args.price))
