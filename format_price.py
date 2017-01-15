b175d661be6b857f8861479425ff4ce80077c7af
SkayLarsen <SkayLarsen@gmail.com>
2017-01-15 14:52:41 +0300

    error message fix

--- format_price.py
+++ format_price.py
@@ -1,15 +1,19 @@
 import argparse
 
+ERROR_MESSAGE = 'Ошибка'
+
+
 def format_price(price):
     price = price.replace(',', '.')
     try:
         price = float(price)
     except ValueError:
-        return 'Ошибка'
+        return ERROR_MESSAGE
     if price.is_integer():
         return '{0:,.0f}'.format(price).replace(',', ' ')
     else:
         return '{0:,.2f}'.format(price).replace(',', ' ')
+
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description='Форматирование записи цены')
